f0b37a106ffdc503dc51dca959d4e73e905d7c00
Utkarsh Raghav <43128146+mrNobody1618@users.noreply.github.com>
2019-05-16 07:26:24 +0530

    Added note
    ##NOTE:##
    #The Target is specified in the arp poison script. This script works in addition to arp-poison. 
    #As, we have to be in middle of the connection to be able to take down or drop the packets.

--- 6. Net Disruptor/net-disrupt.py
+++ 6. Net Disruptor/net-disrupt.py
@@ -16,3 +16,7 @@ subprocess.call(["bash", "-c", "echo 1 > /proc/sys/net/ipv4/ip_forward"])
 queue = netfilterqueue.NetfilterQueue()
 queue.bind(queue_num, process_packet)
 queue.run()
+
+##NOTE:##
+#The Target is specified in the arp poison script. This script works in addition to arp-poison. 
+#As, we have to be in middle of the connection to be able to take down or drop the packets.
